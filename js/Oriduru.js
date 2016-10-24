fafbeaca9b39d2be2fd12f1634ef44e1f10e0ac6
KazuhideOka <kazu1de0ka@gmail.com>
2016-10-25 00:24:35 +0900

    update

--- js/Oriduru.js
+++ js/Oriduru.js
@@ -45,8 +45,8 @@ class Oriduru {
     move(){
         if(touching){
             
-            var dataurl = canvas.toDataURL();
-            document.getElementById("image_png").src = dataurl;
+            location.href = canvas.toDataURL();
+            
             
             if(!this.touch_start && Math.sqrt(Math.pow(touch_x-this.x1,2) + Math.pow(touch_y-this.y1,2)) < 300){
                 this.touch_start = true;
