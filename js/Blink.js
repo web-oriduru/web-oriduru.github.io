731a25131fa76d01000edda225f0e0ddffbd9244
KazuhideOka <kazu1de0ka@gmail.com>
2016-11-01 22:50:25 +0900

    update

--- js/Blink.js
+++ js/Blink.js
@@ -1,14 +1,15 @@
 class Blink{
-    constructor(img,x,y,w,h,max_count) {
+    constructor(img,x,y,w,h,r,max_count) {
         this.img = creatImage(img);
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.r = r;
         this.max_count  = max_count + 0.1;
     }
     draw(){
         var alpha = 55 + 200 * Math.sin(Math.PI * ((timer%this.max_count)/this.max_count));
-        drawImage(this.img, this.x-this.w/2, this.y-this.h/2, this.w, this.h, 0, alpha);
+        drawImage(this.img, this.x-this.w/2, this.y-this.h/2, this.w, this.h, this.r, alpha);
     }
 }
--- js/Paper.js
+++ js/Paper.js
@@ -21,18 +21,46 @@ class Paper {
             img[0] = creatImage("play/play_01_001");
             img[1] = creatImage("play/play_01_002");
             img[2] = creatImage("play/play_01_003");
-            img[3] = creatImage("play/play_01_004");
-            this.blink_arrow = new Blink("play/arrow01",width/2,height/2,width,height,2000);
-            this.blink_touch = new Blink("play/hand01",580,1100,width,height,2000);
+            img[3] = creatImage("play/play_02_001");
+            this.blink_arrow = new Blink("play/arrow01",540,700,270,320,0,2000);
+            this.blink_touch = new Blink("play/hand01",890+20,1100+200,280,440,0,2000);
         }
         else if(num == 2){
             img[0] = img[3];
-            img[1] = creatImage("play/play_02_001");
-            img[2] = creatImage("play/play_02_002");
-            img[3] = creatImage("play/play_02_003");
-            img[4] = creatImage("play/play_02_004");
-            this.blink_arrow = new Blink("play/arrow01",width/2,height/2,width,height,2000);
-            this.blink_touch = new Blink("play/hand01",580,1100,width,height,2000);
+            img[1] = creatImage("play/play_02_002");
+            img[2] = creatImage("play/play_02_003");
+            img[3] = creatImage("play/play_02_004");
+            img[4] = creatImage("play/play_03_001");
+            this.blink_arrow = new Blink("play/arrow02",440,600,270,320,0,2000);
+            this.blink_touch = new Blink("play/hand01",180+20,1110+200,280,440,0,2000);
+        }
+        else if(num == 3){
+            img[0] = img[4];
+            img[1] = creatImage("play/play_03_002");
+            img[2] = creatImage("play/play_03_003");
+            img[3] = creatImage("play/play_03_004");
+            img[4] = creatImage("play/play_04_001");
+            this.blink_arrow = new Blink("play/arrow02",890,830,270,320,120,2000);
+            this.blink_touch = new Blink("play/hand01",890+20,400+200,280,440,0,2000);
+        }
+        else if(num == 4){
+            img[0] = img[4];
+            img[1] = creatImage("play/play_04_002");
+            img[2] = creatImage("play/play_04_003");
+            img[3] = creatImage("play/play_04_004");
+            img[4] = creatImage("play/play_04_005");
+            this.blink_arrow = new Blink("play/arrow01",590,560,270,320,-90,2000);
+            this.blink_touch = new Blink("play/hand01",900+20,460+200,280,440,0,2000);
+        }
+        
+        else if(num == 4){
+            img[0] = img[4];
+            img[1] = creatImage("play/play_04_002");
+            img[2] = creatImage("play/play_04_003");
+            img[3] = creatImage("play/play_04_004");
+            img[4] = creatImage("play/play_04_005");
+            this.blink_arrow = new Blink("play/arrow01",590,560,270,320,-90,2000);
+            this.blink_touch = new Blink("play/hand01",900+20,460+200,280,440,0,2000);
         }
         
         
@@ -42,16 +70,30 @@ class Paper {
         this.num++;
         this.touch_start = false;
         delete this.img;
-        this.setNext(1,890,1100,700,930,false,1);
-        this.setNext(2,this.x2,this.y2,430,660,true,-1);
-        this.setNext(3,this.x2,this.y2,180,410,false,-1);
-        this.setNext(4,this.x2,this.y2,180,410,false,-1);
+        this.setNext( 1,890,1100,700,930,false,1);
+        this.setNext( 2,this.x2,this.y2,430,660,true,-1);
+        this.setNext( 3,this.x2,this.y2,180,410,false,-1);
         
-        this.setNext(5,180,1110,300,900,false,2);
-        this.setNext(6,this.x2,this.y2,530,760,true,-1);
-        this.setNext(7,this.x2,this.y2,610,530,false,-1);
-        this.setNext(8,this.x2,this.y2,880,410,false,-1);
-        this.setNext(9,this.x2,this.y2,880,410,false,-1);
+        this.setNext( 4,180,1110,300,900,false,2);
+        this.setNext( 5,this.x2,this.y2,530,760,true,-1);
+        this.setNext( 6,this.x2,this.y2,680,610,false,-1);
+        this.setNext( 7,this.x2,this.y2,880,410,false,-1);
+        
+        this.setNext( 8,890,400,890,600,false,3);
+        this.setNext( 9,this.x2,this.y2,890,800,true,-1);
+        this.setNext(10,this.x2,this.y2,890,1000,false,-1);
+        this.setNext(11,this.x2,this.y2,890,1200,false,-1);
+        
+        this.setNext(12,900,460,790,500,false,4);
+        this.setNext(13,this.x2,this.y2,620,530,true,-1);
+        this.setNext(14,this.x2,this.y2,410,720,false,-1);
+        if(this.num == 15){
+            this.blink_arrow = new Blink("play/arrow01",400,700,270,320,-120,2000);
+            this.blink_touch = new Blink("play/hand01",410+20,460+200,280,440,0,2000);
+        }
+        this.setNext(15,410,460,410,960,false,-1);
+        
+        this.setNext(16,this.x2,this.y2,410,960,true,-1);
         
     }
     
@@ -90,8 +132,9 @@ class Paper {
     draw(){
         drawImage(this.back_img, 0, 0, 1080, 1920, 0, 255);
         if(img[this.img_num] != null) drawImage(img[this.img_num], 0, 0, 1080, 1920, 0, 255);
-        drawCircle(this.x1,this.y1,100,0,255,0,0.5);
-        drawCircle(this.x2,this.y2,100,0,255,0,0.5);
+        //drawCircle(this.x1,this.y1,100,0,255,0,0.5);
+        //drawCircle(this.x2,this.y2,100,0,255,0,0.5);
+        
         if(this.blink_arrow != null){
             this.blink_arrow.draw();
         }
