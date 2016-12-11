429637cf2332937d97e308ead929f92898274a3a
KazuhideOka <kazu1de0ka@gmail.com>
2016-12-11 16:37:41 +0900

    update

--- js/SendOriduru.js
+++ js/SendOriduru.js
@@ -5,6 +5,8 @@ class SendOriduru{
         this.y = y;
         this.last_x = x;
         this.last_y = y;
+        this.last2_x = x;
+        this.last2_y = y;
         this.center_x = x;
         this.center_y = y;
         this.w = 0;
@@ -27,22 +29,25 @@ class SendOriduru{
         }
         
         if(this.mode == 2){
-            if(this.send_speed<0){
-                this.y+=this.send_speed;
+            if(this.send_speed>0){
+                this.y-=this.send_speed;
                 
             }else{
                 if(touching){
                     this.x = touch_x;
                     this.y = touch_y;
                 }else{
-                    this.x += (this.center_x - this.x)/50;
-                    this.y += (this.center_y - this.y)/50;
-                    if(this.y < this.center_y && this.last_y-this.y < 10){
-                        this.send_speed = this.last_y-this.y;
+                    if(this.y < this.center_y && this.last2_y-this.y > 3){
+                        this.send_speed = this.last2_y-this.y;
+                    }else{
+                        this.x += (this.center_x - this.x)/50;
+                        this.y += (this.center_y - this.y)/50;
                     }
                 }
             }
         
+            this.last2_x = this.last_x;
+            this.last2_y = this.last_y;
             this.last_x = this.x;
             this.last_y = this.y;
         }
@@ -51,8 +56,5 @@ class SendOriduru{
     draw(){
         var alpha = 55 + 200 * Math.sin(Math.PI * ((timer%this.max_count)/this.max_count));
         drawImage(this.img, this.x, this.y, this.w, this.h, this.r, alpha);
-        
-        ctx.fillText("y : "+this.y,50,1400);
-        ctx.fillText("cy : "+this.center_y,50,1500);
     }
 }
